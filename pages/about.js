c67fe533f7a13500777791709c68fe4ea2fa622a
Katarzyna Jaruga <katarzyna.jaruga@codesthq.com>
2018-05-28 00:11:37 +0200

    Create everything :unicorn:

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About</h1>
+      <p>Blah blah</p>
+    </div>
+  </Layout>
+);
+
+export default About;
